9bbcb491613b9dd4613e111222c704ec486071fd
unknown <shaheerahmadtariq@gmail.com>
2022-10-15 10:58:14 +0500

    <With Download Button>

--- mysite/my-app/src/components/Camera.js
+++ mysite/my-app/src/components/Camera.js
@@ -5,8 +5,23 @@ function Camera() {
   const mediaRecorderRef = React.useRef(null);
   const [capturing, setCapturing] = React.useState(false);
   const [recordedChunks, setRecordedChunks] = React.useState([]);
-
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+       var cookies = document.cookie.split(';');
+       for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+             cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+             break;
+          }
+       }
+    }
+    return cookieValue;
+ }
   const handleStartCaptureClick = React.useCallback(() => {
+    console.log("Starting caputure")
     setCapturing(true);
     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
       mimeType: "video/webm"
@@ -26,25 +41,48 @@ function Camera() {
     },
     [setRecordedChunks]
   );
+  const fetchData = React.useCallback(()=>{
+    console.log("Fetching data")
 
+  })
   const handleStopCaptureClick = React.useCallback(() => {
+    console.log("Stopped")
     mediaRecorderRef.current.stop();
     setCapturing(false);
+    console.log("Recorded Chunks length",recordedChunks.length)
   }, [mediaRecorderRef, webcamRef, setCapturing]);
 
   const handleDownload = React.useCallback(() => {
+    // console.log("recordedChunks",recordedChunks.length)
+    let csrftoken = getCookie('csrftoken');
     if (recordedChunks.length) {
       const blob = new Blob(recordedChunks, {
         type: "video/webm"
       });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style = "display: none";
-      a.href = url;
-      a.download = "react-webcam-stream-capture.webm";
-      a.click();
-      window.URL.revokeObjectURL(url);
+      console.log("Blob conatins",blob.arrayBuffer())
+      // converting blob to string
+      // const reader = new FileReader();
+      // reader.addEventListener("loadend", () => {
+      //   // reader.result contains the contents of blob as a typed array
+      // });
+      // reader.readAsArrayBuffer(blob);
+      // const reader = blob.stream()
+      // console.log("Reader: ",reader)
+      var form = new FormData();
+      form.append('video', blob);
+      var url = 'http://127.0.0.1:8000/api/task-create/'
+      console.log("formdata: ",form)
+      fetch(url, {
+        method:'POST',
+        contentType:false,
+        processData: false,
+        data:form
+      }).then((response) =>{
+          console.log("Posted....")
+          fetchData()
+      }).catch(function (e){
+        console.log("Error: ",e)
+      })
       setRecordedChunks([]);
     }
   }, [recordedChunks]);
@@ -56,6 +94,9 @@ function Camera() {
         <button onClick={handleStopCaptureClick}>Stop Capture</button>
       ) : (
         <button onClick={handleStartCaptureClick}>Start Capture</button>
+        )}
+        {recordedChunks.length > 0 && (
+        <button onClick={handleDownload}>Download</button>
       )}
     </>
   )
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # for rest api
+    'corsheaders',  # for cors policies
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'my-app/build/static')
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
